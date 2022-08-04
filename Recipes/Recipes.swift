44b68b617f0d6fb4d3512aeefc0aa04f23724201
PavelGordeyev <gordeyev.p@gmail.com>
2022-08-04 13:43:58 -0700

    Initial Commit

---
+++ Recipes/Recipes.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_List_AppApp.swift
+//  Recipe List App
+//
+//  Created by Pavel Gordeyev on 8/2/22.
+//
+
+import SwiftUI
+
+@main
+struct Recipes: App {
+    var body: some Scene {
+        WindowGroup {
+            RecipeListView()
+        }
+    }
+}
